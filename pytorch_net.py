836882a96f5e727660bdd4ba1fbbba6cec45e3e1
Andrey <and-lukyane@yandex.ru>
2019-01-16 17:43:53 +0300

    Update pytorch_net.py
    Changing net architecture

--- pytorch_net.py
+++ pytorch_net.py
@@ -5,17 +5,17 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 8, 3)
+        self.conv1 = nn.Conv2d(3, 16, 3)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(8, 16, 3)
-        self.fc1 = nn.Linear(576, 128)
-        self.fc2 = nn.Linear(128, 11)
+        self.conv2 = nn.Conv2d(16, 32, 3)
+        self.fc1 = nn.Linear(576 * 2, 512)
+        self.fc2 = nn.Linear(512, 11)
         self.dropout = nn.Dropout(0.1)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 576)
+        x = x.view(-1, 576 * 2)
         x = self.dropout(F.relu(self.fc1(x)))
         x = self.fc2(x)
         return x
